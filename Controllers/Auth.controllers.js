7415452c42871cc9191431ee442e5cbe5b690244
Amaanchaudhary <chaudharyamaan982@gmail.com>
2023-12-07 23:11:12 +0530

    update

--- Controllers/Auth.controllers.js
+++ Controllers/Auth.controllers.js
@@ -1,8 +1,32 @@
 import UserModals from "../Modals/User.modals.js"
 import bcrypt from 'bcrypt';
+import Jwt  from "jsonwebtoken";
 
-export const Login = (req , res) => {
-    res.send("Hello from login")
+export const Login = async (req , res) => {
+    // res.send("Hello from login")
+    try{
+        const {email , password} = req.body.data
+        // console.log(email , password)
+        if(!email || !password ) return res.status(401).json({success : false , message : "All Fields are Mandatory"})
+
+        const user = await UserModals.findOne({email})
+        // console.log(user)
+        if(!user) return res.status(401).json({success : false , message : "Email Not Found"})
+
+        const isPasswordCorrect = await bcrypt.compare(password , user.password)
+
+        if(!isPasswordCorrect){
+            return res.status(401).json({success : false ,message : "Password not matched"})
+        }
+        //generate token
+        const token = await Jwt.sign({id : user._id}, process.env.JWT_SECRET)
+        // console.log(token)
+
+        return res.status(200).json({success : true , message : "Login Successfull" , user : {name : user.name , id : user._id} , token})
+
+    }catch(error){
+        res.status(500).json({success : false , message : error})
+    }
 }
 
 export const Register = async (req , res) => {
@@ -28,5 +52,23 @@ export const Register = async (req , res) => {
 
     }catch(error){
         res.status(500).json({success : false , message : error.message})
+    }
+}
+
+export const getCurrentUser = async (req , res) => {
+    try{
+        const {token} = req.body
+        if(!token) return res.status(401).json({success : false , message : "token is required"})
+
+        const {id} = await Jwt.verify(token , process.env.JWT_SECRET)
+        console.log(id , 'id')
+
+        const user = await UserModals.findById(id)
+        if(!id) return res.status(401).json({success : false , message : "user not found"})
+
+        return res.status(201).json({success : true , user : {name : user.name , id : user._id}})
+
+    }catch(error){
+        res.status(500).json({success : false , message : error.message })
     }
 }
